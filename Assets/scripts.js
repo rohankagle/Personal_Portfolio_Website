5bdc1f0f3fc1323a0676bd6e7c0c33c35638a90e
Rohan Jain <115532248+rohankagle@users.noreply.github.com>
2025-04-02 12:59:56 +0530

    Add files via upload

---
+++ Assets/scripts.js
@@ -0,0 +1,7 @@
+let menuBtn = document.querySelector(".menu")
+let mobileStyle = document.querySelector(".mobile-nav")
+
+menuBtn.addEventListener("click",()=>{
+    mobileStyle.classList.toggle("active")
+   
+})
